8abbbd90440275958ad03e9d38e17ab3e769bf09
MattBurnsUk <hello@matt-burns.com>
2018-12-04 15:58:43 +0800

    made images in thumbBar load in main image when clicked

--- main.js
+++ main.js
@@ -10,7 +10,14 @@ for (let i=1; i<6; i++) {
     var newImage = document.createElement('img');
     newImage.setAttribute('src', '/image-gallery/images/pic'+[i]+'.jpg');
     thumbBar.appendChild(newImage);
-    }
+}
+    
+    function changeImage(e) {
+            var newURL = e.target.getAttribute("src")
+            displayedImage.setAttribute('src', newURL);
+        }
+    
+thumbBar.addEventListener('click', changeImage);
 
 /* Wiring up the Darken/Lighten button */
 
